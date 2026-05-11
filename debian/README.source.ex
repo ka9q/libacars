0f6ac0ca0210a8817842ad54eafe8f991efa5f67
Phil Karn <karn@ka9q.net>
2026-05-10 17:44:54 -0700

    remove unused examples

--- debian/README.source.ex
+++
@@ -1,9 +0,0 @@
-libacars for Debian
-------------------
-
-<This file describes information about the source package, see Debian policy
-manual section 4.14. You WILL either need to modify or delete this file.>
-
-
-
- -- Phil Karn <karn@ka9q.net>  Sun, 10 May 2026 00:14:14 -0700
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibacars 2.2.1 libacars (>> 2.2.1-0), libacars (<< 2.2.1-99)
